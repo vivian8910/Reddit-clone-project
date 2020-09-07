532bad7d180141a53b0807541453d38bf20ac584
yangx <james891013@outlook.com>
2020-09-07 10:02:03 +0200

    add register and login

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import path from 'path';
 import { MikroORM } from '@mikro-orm/core';
+import { User } from './entities/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'redditclone',
   user: 'postgres',
   password: 'postgres',
