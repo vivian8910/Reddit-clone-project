7e5ded764f38cbd79b5148f59f6c95f5b7d10d8e
vivian8910 <james891013@outlook.com>
2020-10-29 16:48:29 +0100

    set up urql client and ssr

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,17 @@
+import { withUrqlClient } from 'next-urql';
 import { NavBar } from '../components/NavBar';
+import { usePostsQuery } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>hello</div>
+      {!data ? <div>loading...</div> : data.posts.map((post) => <div key={post.id}>{post.title}</div>)}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -6,6 +6,8 @@ import { Box, Button } from '@chakra-ui/core';
 import { useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { withUrqlClient } from 'next-urql';
 
 interface registerProps {}
 
@@ -41,4 +43,4 @@ export const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default withUrqlClient(createUrqlClient)(Register);
